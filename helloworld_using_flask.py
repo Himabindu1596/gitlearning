1bea53bd84ff20b6c99ba75b94e0cb92352c122b
Himabindu1596 <himabindukalluri15@gmail.com>
2025-09-13 14:49:30 -0700

    initial commit

---
+++ helloworld_using_flask.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def Hello():
+    return "welcome"
+@app.route('/welcome/<name>')
+def Welcome(name):
+    return f"Welcome {name}"
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 8800)
